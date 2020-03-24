feb89dc75a8a04ecedb9f54476a31b54f9557f35
Ertugrul2070 <e.karaduman2070@gmail.com>
2020-03-24 16:23:26 +0100

    a matiie

--- PopcornPlaza/Program.cs
+++ PopcornPlaza/Program.cs
@@ -6,7 +6,7 @@ namespace PopcornPlaza
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            Console.WriteLine("Hello World a mattie!");
             Console.WriteLine("KRIJG CORONA A MATTIE!! hallo");
         }
     }
