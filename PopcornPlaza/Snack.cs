aa960741a4eccc137c070b9e4aa90cb013dd454d
Eren Gul <Eren_gul1@hotmail.com>
2020-03-25 10:39:29 +0100

    classes project aangemaakt

---
+++ PopcornPlaza/Snack.cs
@@ -0,0 +1,34 @@
+﻿namespace PopcornPlaza
+{
+    public class Snack
+    {
+        private int ID;
+
+        private string name;
+
+        private string price;
+
+        private string type;
+
+        public void getID()
+        {
+
+        }
+
+        public string getName()
+        {
+            return null;
+        }
+
+        public string getPrice()
+        {
+            return null;
+        }
+
+        public string getType()
+        {
+            return null;
+        }
+
+    }
+}
---
+++ PopcornPlaza/movie.cs
@@ -0,0 +1,47 @@
+﻿namespace PopcornPlaza
+{
+    public class movie
+    {
+        private string title;
+
+        private string description;
+
+        private int age;
+
+        private object img;
+
+        private DateTime dateTime;
+
+        private Reservation reservation;
+
+        private Reservation reservation;
+
+        private cinimaRoom cinimaRoom;
+
+        public string getTitle()
+        {
+            return null;
+        }
+
+        public string getDescription()
+        {
+            return null;
+        }
+
+        public int getAge()
+        {
+            return 0;
+        }
+
+        public object getImg()
+        {
+            return null;
+        }
+
+        public DateTime getDateTime()
+        {
+            return null;
+        }
+
+    }
+}
