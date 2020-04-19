f193e18bfa733dda6c4f6d2a62f16525a9fad687
Eren Gul <erengul321@gmail.com>
2020-04-19 14:01:04 +0200

    ReservationAdministration aangepast zodat alles vanaf hier gaat

--- Engine/Models/ReservationAdministration.cs
+++ Engine/Models/ReservationAdministration.cs
@@ -4,11 +4,19 @@ namespace Engine.Models
 {
     public class ReservationAdministration
     {
-        public string[] custommers { get; set; }
-        public string[] reservations { get; set; }
+        public string[] custommers;
+        public string[] reservations;
 
+        public ReservationAdministration()
+        {
+            
+        }
 
-
+        public void addCustomer(string email, string gender, string name, string surname, string streetadress, string postcode, string city,
+            int phonenumber)
+        {
+            Custommer cust = new Custommer(email, gender, name, surname, streetadress, postcode, city, phonenumber);
+        }
 
     }
 }
