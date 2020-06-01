451c42cf7997971fa8878f96b62de542fcfcf88f
Ertugrul2070 <e.karaduman2070@gmail.com>
2020-06-02 00:25:47 +0200

    load charts in via code

--- UI/Administratie/AdminMain.Designer.cs
+++ UI/Administratie/AdminMain.Designer.cs
@@ -28,31 +28,36 @@
         /// </summary>
         private void InitializeComponent()
         {
-            System.Windows.Forms.DataVisualization.Charting.ChartArea chartArea2 = new System.Windows.Forms.DataVisualization.Charting.ChartArea();
-            System.Windows.Forms.DataVisualization.Charting.Legend legend2 = new System.Windows.Forms.DataVisualization.Charting.Legend();
-            System.Windows.Forms.DataVisualization.Charting.Series series2 = new System.Windows.Forms.DataVisualization.Charting.Series();
             System.Windows.Forms.DataVisualization.Charting.ChartArea chartArea1 = new System.Windows.Forms.DataVisualization.Charting.ChartArea();
             System.Windows.Forms.DataVisualization.Charting.Legend legend1 = new System.Windows.Forms.DataVisualization.Charting.Legend();
             System.Windows.Forms.DataVisualization.Charting.Series series1 = new System.Windows.Forms.DataVisualization.Charting.Series();
+            System.Windows.Forms.DataVisualization.Charting.ChartArea chartArea2 = new System.Windows.Forms.DataVisualization.Charting.ChartArea();
+            System.Windows.Forms.DataVisualization.Charting.Legend legend2 = new System.Windows.Forms.DataVisualization.Charting.Legend();
+            System.Windows.Forms.DataVisualization.Charting.Series series2 = new System.Windows.Forms.DataVisualization.Charting.Series();
             this.panel1 = new System.Windows.Forms.Panel();
-            this.SnacksBtn = new System.Windows.Forms.Button();
+            this.selectPanel = new System.Windows.Forms.Panel();
             this.LoguitBtn = new System.Windows.Forms.Button();
+            this.SnacksBtn = new System.Windows.Forms.Button();
             this.FilmsBtn = new System.Windows.Forms.Button();
             this.GebruikersBtn = new System.Windows.Forms.Button();
             this.PaneelBtn = new System.Windows.Forms.Button();
-            this.selectPanel = new System.Windows.Forms.Panel();
             this.containerPanel = new System.Windows.Forms.Panel();
-            this.panelPie1 = new System.Windows.Forms.Panel();
-            this.panelPie2 = new System.Windows.Forms.Panel();
             this.panelChart = new System.Windows.Forms.Panel();
-            this.pieChart1 = new System.Windows.Forms.DataVisualization.Charting.Chart();
+            this.panelPie2 = new System.Windows.Forms.Panel();
             this.pieChart2 = new System.Windows.Forms.DataVisualization.Charting.Chart();
+            this.panelPie1 = new System.Windows.Forms.Panel();
+            this.pieChart1 = new System.Windows.Forms.DataVisualization.Charting.Chart();
+            this.RTBdata = new System.Windows.Forms.RichTextBox();
+            this.listView1 = new System.Windows.Forms.ListView();
+            this.chName = ((System.Windows.Forms.ColumnHeader)(new System.Windows.Forms.ColumnHeader()));
+            this.cLastName = ((System.Windows.Forms.ColumnHeader)(new System.Windows.Forms.ColumnHeader()));
             this.panel1.SuspendLayout();
             this.containerPanel.SuspendLayout();
-            this.panelPie1.SuspendLayout();
+            this.panelChart.SuspendLayout();
             this.panelPie2.SuspendLayout();
-            ((System.ComponentModel.ISupportInitialize)(this.pieChart1)).BeginInit();
             ((System.ComponentModel.ISupportInitialize)(this.pieChart2)).BeginInit();
+            this.panelPie1.SuspendLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.pieChart1)).BeginInit();
             this.SuspendLayout();
             // 
             // panel1
@@ -70,21 +75,13 @@
             this.panel1.Size = new System.Drawing.Size(200, 461);
             this.panel1.TabIndex = 0;
             // 
-            // SnacksBtn
+            // selectPanel
             // 
-            this.SnacksBtn.FlatAppearance.BorderSize = 0;
-            this.SnacksBtn.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
-            this.SnacksBtn.Font = new System.Drawing.Font("Century Gothic", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.SnacksBtn.Image = global::UI.Properties.Resources.snack;
-            this.SnacksBtn.Location = new System.Drawing.Point(0, 225);
-            this.SnacksBtn.Name = "SnacksBtn";
-            this.SnacksBtn.Size = new System.Drawing.Size(200, 75);
-            this.SnacksBtn.TabIndex = 3;
-            this.SnacksBtn.Text = "Snacks";
-            this.SnacksBtn.TextAlign = System.Drawing.ContentAlignment.BottomCenter;
-            this.SnacksBtn.TextImageRelation = System.Windows.Forms.TextImageRelation.ImageAboveText;
-            this.SnacksBtn.UseVisualStyleBackColor = true;
-            this.SnacksBtn.Click += new System.EventHandler(this.SnacksBtn_Click);
+            this.selectPanel.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(46)))), ((int)(((byte)(45)))), ((int)(((byte)(41)))));
+            this.selectPanel.Location = new System.Drawing.Point(174, 11);
+            this.selectPanel.Name = "selectPanel";
+            this.selectPanel.Size = new System.Drawing.Size(10, 50);
+            this.selectPanel.TabIndex = 1;
             // 
             // LoguitBtn
             // 
@@ -100,6 +97,22 @@
             this.LoguitBtn.TextAlign = System.Drawing.ContentAlignment.BottomCenter;
             this.LoguitBtn.TextImageRelation = System.Windows.Forms.TextImageRelation.ImageAboveText;
             this.LoguitBtn.UseVisualStyleBackColor = true;
+            // 
+            // SnacksBtn
+            // 
+            this.SnacksBtn.FlatAppearance.BorderSize = 0;
+            this.SnacksBtn.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.SnacksBtn.Font = new System.Drawing.Font("Century Gothic", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.SnacksBtn.Image = global::UI.Properties.Resources.snack;
+            this.SnacksBtn.Location = new System.Drawing.Point(0, 225);
+            this.SnacksBtn.Name = "SnacksBtn";
+            this.SnacksBtn.Size = new System.Drawing.Size(200, 75);
+            this.SnacksBtn.TabIndex = 3;
+            this.SnacksBtn.Text = "Snacks";
+            this.SnacksBtn.TextAlign = System.Drawing.ContentAlignment.BottomCenter;
+            this.SnacksBtn.TextImageRelation = System.Windows.Forms.TextImageRelation.ImageAboveText;
+            this.SnacksBtn.UseVisualStyleBackColor = true;
+            this.SnacksBtn.Click += new System.EventHandler(this.SnacksBtn_Click);
             // 
             // FilmsBtn
             // 
@@ -149,14 +162,6 @@
             this.PaneelBtn.UseVisualStyleBackColor = true;
             this.PaneelBtn.Click += new System.EventHandler(this.PaneelBtn_Click);
             // 
-            // selectPanel
-            // 
-            this.selectPanel.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(46)))), ((int)(((byte)(45)))), ((int)(((byte)(41)))));
-            this.selectPanel.Location = new System.Drawing.Point(174, 11);
-            this.selectPanel.Name = "selectPanel";
-            this.selectPanel.Size = new System.Drawing.Size(10, 50);
-            this.selectPanel.TabIndex = 1;
-            // 
             // containerPanel
             // 
             this.containerPanel.Controls.Add(this.panelChart);
@@ -167,13 +172,14 @@
             this.containerPanel.Size = new System.Drawing.Size(548, 437);
             this.containerPanel.TabIndex = 1;
             // 
-            // panelPie1
+            // panelChart
             // 
-            this.panelPie1.Controls.Add(this.pieChart1);
-            this.panelPie1.Location = new System.Drawing.Point(3, 3);
-            this.panelPie1.Name = "panelPie1";
-            this.panelPie1.Size = new System.Drawing.Size(268, 218);
-            this.panelPie1.TabIndex = 0;
+            this.panelChart.Controls.Add(this.listView1);
+            this.panelChart.Controls.Add(this.RTBdata);
+            this.panelChart.Location = new System.Drawing.Point(3, 227);
+            this.panelChart.Name = "panelChart";
+            this.panelChart.Size = new System.Drawing.Size(542, 207);
+            this.panelChart.TabIndex = 1;
             // 
             // panelPie2
             // 
@@ -183,12 +189,32 @@
             this.panelPie2.Size = new System.Drawing.Size(268, 218);
             this.panelPie2.TabIndex = 1;
             // 
-            // panelChart
+            // pieChart2
             // 
-            this.panelChart.Location = new System.Drawing.Point(3, 227);
-            this.panelChart.Name = "panelChart";
-            this.panelChart.Size = new System.Drawing.Size(542, 207);
-            this.panelChart.TabIndex = 1;
+            this.pieChart2.BorderlineColor = System.Drawing.Color.FromArgb(((int)(((byte)(248)))), ((int)(((byte)(210)))), ((int)(((byte)(65)))));
+            this.pieChart2.BorderlineDashStyle = System.Windows.Forms.DataVisualization.Charting.ChartDashStyle.Solid;
+            chartArea1.Name = "ChartArea1";
+            this.pieChart2.ChartAreas.Add(chartArea1);
+            legend1.Name = "Legend1";
+            this.pieChart2.Legends.Add(legend1);
+            this.pieChart2.Location = new System.Drawing.Point(3, 3);
+            this.pieChart2.Name = "pieChart2";
+            series1.ChartArea = "ChartArea1";
+            series1.ChartType = System.Windows.Forms.DataVisualization.Charting.SeriesChartType.Pie;
+            series1.Legend = "Legend1";
+            series1.Name = "Pie2";
+            this.pieChart2.Series.Add(series1);
+            this.pieChart2.Size = new System.Drawing.Size(262, 207);
+            this.pieChart2.TabIndex = 1;
+            this.pieChart2.Text = "chart1";
+            // 
+            // panelPie1
+            // 
+            this.panelPie1.Controls.Add(this.pieChart1);
+            this.panelPie1.Location = new System.Drawing.Point(3, 3);
+            this.panelPie1.Name = "panelPie1";
+            this.panelPie1.Size = new System.Drawing.Size(268, 218);
+            this.panelPie1.TabIndex = 0;
             // 
             // pieChart1
             // 
@@ -209,24 +235,39 @@
             this.pieChart1.TabIndex = 0;
             this.pieChart1.Text = "chart1";
             // 
-            // pieChart2
+            // RTBdata
             // 
-            this.pieChart2.BorderlineColor = System.Drawing.Color.FromArgb(((int)(((byte)(248)))), ((int)(((byte)(210)))), ((int)(((byte)(65)))));
-            this.pieChart2.BorderlineDashStyle = System.Windows.Forms.DataVisualization.Charting.ChartDashStyle.Solid;
-            chartArea1.Name = "ChartArea1";
-            this.pieChart2.ChartAreas.Add(chartArea1);
-            legend1.Name = "Legend1";
-            this.pieChart2.Legends.Add(legend1);
-            this.pieChart2.Location = new System.Drawing.Point(6, 3);
-            this.pieChart2.Name = "pieChart2";
-            series1.ChartArea = "ChartArea1";
-            series1.ChartType = System.Windows.Forms.DataVisualization.Charting.SeriesChartType.Pie;
-            series1.Legend = "Legend1";
-            series1.Name = "Pie2";
-            this.pieChart2.Series.Add(series1);
-            this.pieChart2.Size = new System.Drawing.Size(262, 207);
-            this.pieChart2.TabIndex = 1;
-            this.pieChart2.Text = "chart1";
+            this.RTBdata.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(46)))), ((int)(((byte)(45)))), ((int)(((byte)(41)))));
+            this.RTBdata.BorderStyle = System.Windows.Forms.BorderStyle.None;
+            this.RTBdata.Font = new System.Drawing.Font("Century Gothic", 12F, System.Drawing.FontStyle.Bold);
+            this.RTBdata.Location = new System.Drawing.Point(3, 3);
+            this.RTBdata.Name = "RTBdata";
+            this.RTBdata.Size = new System.Drawing.Size(536, 201);
+            this.RTBdata.TabIndex = 0;
+            this.RTBdata.Text = "";
+            // 
+            // listView1
+            // 
+            this.listView1.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(46)))), ((int)(((byte)(45)))), ((int)(((byte)(41)))));
+            this.listView1.BorderStyle = System.Windows.Forms.BorderStyle.None;
+            this.listView1.Columns.AddRange(new System.Windows.Forms.ColumnHeader[] {
+            this.chName,
+            this.cLastName});
+            this.listView1.HideSelection = false;
+            this.listView1.Location = new System.Drawing.Point(3, 3);
+            this.listView1.Name = "listView1";
+            this.listView1.Size = new System.Drawing.Size(536, 201);
+            this.listView1.TabIndex = 1;
+            this.listView1.UseCompatibleStateImageBehavior = false;
+            this.listView1.View = System.Windows.Forms.View.Details;
+            // 
+            // chName
+            // 
+            this.chName.Text = "Name";
+            // 
+            // cLastName
+            // 
+            this.cLastName.Text = "Lastname";
             // 
             // AdminMain
             // 
@@ -240,10 +281,11 @@
             this.Text = "AdminMain";
             this.panel1.ResumeLayout(false);
             this.containerPanel.ResumeLayout(false);
-            this.panelPie1.ResumeLayout(false);
+            this.panelChart.ResumeLayout(false);
             this.panelPie2.ResumeLayout(false);
-            ((System.ComponentModel.ISupportInitialize)(this.pieChart1)).EndInit();
             ((System.ComponentModel.ISupportInitialize)(this.pieChart2)).EndInit();
+            this.panelPie1.ResumeLayout(false);
+            ((System.ComponentModel.ISupportInitialize)(this.pieChart1)).EndInit();
             this.ResumeLayout(false);
 
         }
@@ -263,5 +305,9 @@
         private System.Windows.Forms.Panel panelPie1;
         private System.Windows.Forms.DataVisualization.Charting.Chart pieChart1;
         private System.Windows.Forms.DataVisualization.Charting.Chart pieChart2;
+        private System.Windows.Forms.RichTextBox RTBdata;
+        private System.Windows.Forms.ListView listView1;
+        private System.Windows.Forms.ColumnHeader chName;
+        private System.Windows.Forms.ColumnHeader cLastName;
     }
 }
--- UI/Administratie/AdminMain.cs
+++ UI/Administratie/AdminMain.cs
@@ -9,6 +9,8 @@ using System.Threading.Tasks;
 using System.Windows.Forms;
 using Engine.ViewModels;
 using MySql.Data.MySqlClient;
+using MySqlX.XDevAPI.Relational;
+using System.Windows.Forms.DataVisualization.Charting;
 
 namespace UI.Administratie
 {
@@ -20,13 +22,33 @@ namespace UI.Administratie
         public AdminMain()
         {
             InitializeComponent();
+
+            PaneelBtn_Click(null, new EventArgs());
+
+        }
+
+        private void PaneelBtn_Click(object sender, EventArgs e)
+        {
+            
+            selectPanel.Height = PaneelBtn.Height - 25;
+            selectPanel.Top = PaneelBtn.Top + 11;
+
+            containerPanel.Controls.Clear();
+
+            Panel p1 = CreatePanel(268, 218, 3, 3, "panelPie1");
+            Panel p2 = CreatePanel(268, 218, 3, 277, "panelPie2");
+
+            containerPanel.Controls.Add(p1);
+            containerPanel.Controls.Add(p2);
+
+            //Load data for chart 1
             for (int i = 1; i < 7; i++)
             {
+
                 string selectQuery = "SELECT COUNT(genre_idgenre) FROM mydb.movies WHERE genre_idgenre = " + i;
                 try
                 {
                     dbc.cnn.Open();
-
 
                     MySqlCommand command = new MySqlCommand(selectQuery, dbc.cnn);
 
@@ -38,6 +60,7 @@ namespace UI.Administratie
                         CountGenres.Add(genreName);
                     }
 
+
                 }
                 catch (Exception)
                 {
@@ -49,27 +72,20 @@ namespace UI.Administratie
                 }
 
             }
+            var genres = new List<string> { "Science Fiction", "Adventure", "Horror", "Comedy", "Fantasy", "Romance" };
+            Chart c1 = CreateChart(262, 207, 3, 3, "pieChart1", "Pie1", CountGenres, genres);
+            c1.Parent = p1;
 
-            pieChart1.Titles.Add("Film Genres");
-
-            pieChart1.Series["Pie1"].Points.AddXY("Science Fiction", CountGenres[0]);
-            pieChart1.Series["Pie1"].Points.AddXY("Adventure", CountGenres[1]);
-            pieChart1.Series["Pie1"].Points.AddXY("Horror", CountGenres[2]);
-            pieChart1.Series["Pie1"].Points.AddXY("Comedy", CountGenres[3]);
-            pieChart1.Series["Pie1"].Points.AddXY("Fantasy", CountGenres[4]);
-            pieChart1.Series["Pie1"].Points.AddXY("Romance", CountGenres[5]);
-
-            var genders = new List<string> { "Man", "Vrouw", "Anders" , "Wil ik niet zeggen"};
+            //Load data for chart 2
+            var genders = new List<string> { "Man", "Vrouw", "Anders", "Wil ik niet zeggen" };
             for (int i = 0; i < 4; i++)
             {
-                string selectQuery = "SELECT COUNT(idUsers) FROM mydb.customers WHERE gender = '" + genders[i] +"'";
+                string selectQuery = "SELECT COUNT(idUsers) FROM mydb.customers WHERE gender = '" + genders[i] + "'";
                 try
                 {
                     dbc.cnn.Open();
 
-
                     MySqlCommand command = new MySqlCommand(selectQuery, dbc.cnn);
-
                     MySqlDataReader dataReader = command.ExecuteReader();
 
                     while (dataReader.Read())
@@ -78,6 +94,7 @@ namespace UI.Administratie
                         CountGenders.Add(gender);
                     }
 
+
                 }
                 catch (Exception)
                 {
@@ -89,39 +106,249 @@ namespace UI.Administratie
                 }
 
             }
-            int cGen = 0;
-            pieChart2.Titles.Add("Gebruiker geslacht");
-            foreach (string gender in genders)
-            {
-                pieChart2.Series["Pie2"].Points.AddXY(gender, CountGenders[cGen]);
-                cGen++;
-            }
-            
-
-        }
-
-        private void PaneelBtn_Click(object sender, EventArgs e)
-        {
-            selectPanel.Height = PaneelBtn.Height - 25;
-            selectPanel.Top = PaneelBtn.Top + 11;
+            Chart c2 = CreateChart(262, 207, 3, 3, "pieChart2", "Pie2", CountGenders, genders);
+            c2.Parent = p2;
+            c2.Show();
         }
 
         private void GebruikersBtn_Click(object sender, EventArgs e)
         {
+            
             selectPanel.Height = GebruikersBtn.Height - 25;
             selectPanel.Top = GebruikersBtn.Top + 11;
+            panelPie1.Visible = false;
+            panelPie2.Visible = false;
+            panelChart.Visible = false;
+            containerPanel.Controls.Clear();
+            ListView CLW = CreateListView(containerPanel.Height, containerPanel.Width);
+            CLW.Clear();
+            containerPanel.Controls.Add(CLW);
+
+            string selectQuery = "SELECT * FROM mydb.customers";
+
+            List<string> cName = new List<string>();
+            List<string> cLast = new List<string>();
+            List<string> cGender = new List<string>();
+            List<string> cEmail = new List<string>();
+            try
+            {
+                dbc.cnn.Open();
+
+                MySqlCommand command = new MySqlCommand(selectQuery, dbc.cnn);
+                
+                MySqlDataReader dataReader = command.ExecuteReader();
+                int custCount = 0;
+                while (dataReader.Read())
+                {
+                    string custName = dataReader.GetString("firstName");
+                    string custLastName = dataReader.GetString("lastName");
+                    string gender = dataReader.GetString("gender");
+                    string email = dataReader.GetString("email");
+                    cName.Add(custName);
+                    cLast.Add(custLastName);
+                    cGender.Add(gender);
+                    cEmail.Add(email); 
+                    custCount++;
+                }
+
+                CLW.Columns.Add("Naam", 200);
+                CLW.Columns.Add("Achternaam", 200);
+                CLW.Columns.Add("Geslacht", 200);
+                CLW.Columns.Add("Email", 200);
+
+                for (int i = 0; i < custCount; i++)
+                {   
+                    ListViewItem item = new ListViewItem(cName[i]);
+                    item.SubItems.Add(cLast[i]);
+                    item.SubItems.Add(cGender[i]);
+                    item.SubItems.Add(cEmail[i]);
+                    CLW.Items.Add(item);
+                }
+            }
+            catch (Exception)
+            {
+                throw;
+            }
+            finally
+            {
+                dbc.cnn.Close();
+            }
+
         }
 
         private void FilmsBtn_Click(object sender, EventArgs e)
-        {
+        {   
             selectPanel.Height = FilmsBtn.Height - 25;
             selectPanel.Top = FilmsBtn.Top + 11;
+            panelPie1.Visible = false;
+            panelPie2.Visible = false;
+            panelChart.Visible = false;
+            containerPanel.Controls.Clear();
+            ListView CLW = CreateListView(containerPanel.Height, containerPanel.Width);
+            containerPanel.Controls.Add(CLW);
+
+            string selectQuery = "SELECT * FROM mydb.movies INNER JOIN mydb.Genre ON mydb.movies.genre_idgenre=mydb.Genre.idgenre;";
+
+            List<string> mName = new List<string>();
+            List<string> mReleaseDate = new List<string>();
+            List<string> mPegi = new List<string>();
+            List<string> mGenre = new List<string>();
+            try
+            {
+                dbc.cnn.Open();
+
+                MySqlCommand command = new MySqlCommand(selectQuery, dbc.cnn);
+
+                MySqlDataReader dataReader = command.ExecuteReader();
+                int custCount = 0;
+                while (dataReader.Read())
+                {
+                    string movName = dataReader.GetString("name");
+                    string movReleaseDate = dataReader.GetString("releaseDate");
+                    string movPegi = dataReader.GetString("pegi");
+                    string movGenre = dataReader.GetString("genreName");
+                    mName.Add(movName);
+                    mReleaseDate.Add(movReleaseDate);
+                    mPegi.Add(movPegi);
+                    mGenre.Add(movGenre);
+                    custCount++;
+
+                }
+
+                CLW.Columns.Add("Naam", 200);
+                CLW.Columns.Add("Uitgebracht op", 200);
+                CLW.Columns.Add("Pegi", 200);
+                CLW.Columns.Add("Genre", 200);
+
+                for (int i = 0; i < custCount; i++)
+                {
+                    ListViewItem item = new ListViewItem(mName[i]);
+                    item.SubItems.Add(mReleaseDate[i]);
+                    item.SubItems.Add(mPegi[i] + " +");
+                    item.SubItems.Add(mGenre[i]);
+                    CLW.Items.Add(item);
+                }
+            }
+            catch (Exception)
+            {
+                throw;
+            }
+            finally
+            {
+                dbc.cnn.Close();
+            }
         }
 
         private void SnacksBtn_Click(object sender, EventArgs e)
         {
             selectPanel.Height = SnacksBtn.Height - 25;
             selectPanel.Top = SnacksBtn.Top + 15;
+            panelPie1.Visible = false;
+            panelChart.Visible = false;
+            panelPie2.Visible = false;
+            containerPanel.Controls.Clear();
+            ListView CLW = CreateListView(containerPanel.Height, containerPanel.Width);
+            containerPanel.Controls.Add(CLW);
+
+            string selectQuery = "SELECT * FROM mydb.snackcatelogus";
+
+            List<string> sName = new List<string>();
+            List<string> sPrice = new List<string>();
+            List<string> sType = new List<string>();
+            try
+            {
+                dbc.cnn.Open();
+
+                MySqlCommand command = new MySqlCommand(selectQuery, dbc.cnn);
+
+                MySqlDataReader dataReader = command.ExecuteReader();
+                int custCount = 0;
+                while (dataReader.Read())
+                {
+                    string snacName = dataReader.GetString("name");
+                    string snacPrice = dataReader.GetString("prijs");
+                    string snacType = dataReader.GetString("type");
+                    sName.Add(snacName);
+                    sPrice.Add(snacPrice);
+                    sType.Add(snacType);
+                    custCount++;
+
+                }
+
+                CLW.Columns.Add("Naam", 200);
+                CLW.Columns.Add("Prijs", 200);
+                CLW.Columns.Add("Type", 200);
+
+                for (int i = 0; i < custCount; i++)
+                {
+                    ListViewItem item = new ListViewItem(sName[i]);
+                    item.SubItems.Add(sPrice[i]);
+                    item.SubItems.Add(sType[i]);
+                    CLW.Items.Add(item);
+                }
+            }
+            catch (Exception)
+            {
+                throw;
+            }
+            finally
+            {
+                dbc.cnn.Close();
+            }
+        }
+
+            ListView CreateListView(int Height, int Width)
+        {
+            ListView lv1 = new ListView();
+
+            lv1.Font = new Font("Century Gothic", 12, FontStyle.Bold);
+            lv1.Location = new Point(3, 3);
+            lv1.Name = "custData";
+            lv1.Size = new Size(Width - 20, Height - 20);
+            lv1.View = View.Details;
+            lv1.FullRowSelect = true;
+            lv1.BackColor = Color.FromArgb(46, 45, 41);
+            lv1.BorderStyle = BorderStyle.None;
+            lv1.ForeColor = Color.White;
+
+            return lv1;
+        }
+
+        Panel CreatePanel(int height, int width, int left, int top, string name)
+        {
+            Panel pnl = new Panel();
+
+            pnl.Name = name;
+            pnl.Size = new Size(height, width);
+            pnl.Location = new Point(top, left);
+            pnl.BackColor = Color.FromArgb(46, 45, 41);
+
+            return pnl;
+        }
+
+        Chart CreateChart(int height, int width, int left, int top, string name, string serieName,  List<string> value, List<string> title)
+        {
+            Chart chrt = new Chart();
+
+            chrt.Name = name;
+            chrt.Titles.Add("aaaaaaaaaaaaaaaaaaaa");
+            chrt.Size = new Size(height, width);
+            chrt.Location = new Point(top, left);
+            chrt.Series.Add(serieName);
+            chrt.Series[serieName].ChartType = SeriesChartType.Pie;
+            chrt.Series[serieName].SetDefault(true);
+            chrt.Series[serieName].Enabled = true;
+            chrt.Visible = true;
+
+            int cGen = 0;
+            foreach (string val in value)
+            {
+                chrt.Series[serieName].Points.AddXY(title[cGen], val);
+                cGen++;
+            }
+            
+
+            return chrt;
         }
     }
 }
